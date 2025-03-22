605e88ebc9ef8be7385e7ed72b3d5f7607425916
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2025-03-22 19:00:51 +0530

    Create Valid_Parentheses.java

---
+++ Valid_Parentheses.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class Valid_Parentheses {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+    }
+    public static boolean isValid(String s){
+        Stack<Character> st = new Stack<>();
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                else if(ch==')'){
+                    if(st.peek()!='('){
+                        return false;
+                    }
+                }
+                else if(ch=='}'){
+                    if(st.peek()!='{'){
+                        return false;
+                    }
+            }
+            else if(ch==']'){
+                if(st.peek()!='['){
+                    return false;
+                }
+        }
+        st.pop();
+    }
+}
+return st.isEmpty();
+}
+}
